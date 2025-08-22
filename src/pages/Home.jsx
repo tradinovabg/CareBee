a013448c123d8e8ed1e4867cd18fe6afaefa7f63
tradinovabg <tradinovabg@gmail.com>
2025-08-22 20:56:50 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,14 @@ import { useTranslation } from 'react-i18next'
 
 export default function Home() {
   const { t } = useTranslation()
-  return (
-import { Link } from 'react-router-dom'
-import { useTranslation } from 'react-i18next'
 
-export default function Home() {
-  const { t } = useTranslation()
   return (
     <div className="home">
       <h1>{t('title', 'CareBee')}</h1>
       <p>{t('tagline', 'Quick help at your fingertips.')}</p>
+
       <Link to="/sos">
         <button className="sos-button">{t('goToSOS', 'SOS')}</button>
-      </Link>
-    </div>
-  )
-}
- main
       </Link>
     </div>
   )
