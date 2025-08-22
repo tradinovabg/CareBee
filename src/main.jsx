e282855cbe1b2878376862cb811b05043ab661bc
tradinovabg <tradinovabg@gmail.com>
2025-08-22 18:27:14 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import './i18n.js' // ВАЖНО: инициализация переводов
-
+import './i18n.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
