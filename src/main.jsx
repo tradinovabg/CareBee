ee1e74f620dce506d935e73dacc57dc927f4e892
tradinovabg <tradinovabg@gmail.com>
2025-08-22 18:16:21 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { I18nextProvider } from 'react-i18next'
-import './index.css'
 import App from './App.jsx'
-import i18n from './i18n.js'
+import './index.css'
+import './i18n.js' // ВАЖНО: инициализация переводов
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <App />
-      </BrowserRouter>
-    </I18nextProvider>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
