8b4f944afc9b2ce453df606b59252b959c16bbed
tradinovabg <tradinovabg@gmail.com>
2025-08-22 18:27:42 +0200

    Update LanguageSwitcher.jsx

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,10 +1,8 @@
 import { useTranslation } from 'react-i18next'
-
 export default function LanguageSwitcher() {
   const { i18n } = useTranslation()
-  const change = (e) => i18n.changeLanguage(e.target.value)
   return (
-    <select onChange={change} value={i18n.language}>
+    <select onChange={(e) => i18n.changeLanguage(e.target.value)} value={i18n.language}>
       <option value="en">EN</option>
       <option value="ru">RU</option>
       <option value="fr">FR</option>
