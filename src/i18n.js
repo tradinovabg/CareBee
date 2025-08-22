d4a56ada482dadbdad5ffbf9836a51147b8566c2
tradinovabg <tradinovabg@gmail.com>
2025-08-22 18:28:23 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -3,17 +3,9 @@ import { initReactI18next } from 'react-i18next'
 import en from './locales/en.json'
 import ru from './locales/ru.json'
 import fr from './locales/fr.json'
-
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: { translation: en },
-      ru: { translation: ru },
-      fr: { translation: fr },
-    },
-    fallbackLng: 'en',
-    interpolation: { escapeValue: false },
-  })
-
+i18n.use(initReactI18next).init({
+  resources: { en: { translation: en }, ru: { translation: ru }, fr: { translation: fr } },
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false },
+})
 export default i18n
