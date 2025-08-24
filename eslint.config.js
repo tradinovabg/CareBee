237f34b823d4e5ec27f4fc86edb0184c0a171369
tradinovabg <tradinovabg@gmail.com>
2025-08-25 00:09:36 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,24 @@
-// eslint.config.js
-import js from '@eslint/js';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import globals from 'globals';
+// flat-config
+import js from '@eslint/js'
+import globals from 'globals'
 
 export default [
-  // База от @eslint/js
   js.configs.recommended,
 
-  // Конфиг для обычного кода (браузер + JSX)
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: { ecmaFeatures: { jsx: true } },
-      globals: {
-        ...globals.browser, // window, document и т.п.
-      },
-    },
-    plugins: { react, 'react-hooks': reactHooks },
-    rules: {
-      'react/react-in-jsx-scope': 'off', // React 17+
-      'react/prop-types': 'off',
-    },
-    settings: { react: { version: 'detect' } },
-  },
+  // ваш основной конфиг (если есть)…
 
-  // Переопределения для тестов (Node-глобалы, включая process)
+  // тесты запускаются в node:
   {
-    files: ['**/*.test.{js,jsx}', 'test/**/*.{js,jsx}'],
+    files: ['test/**/*', '**/*.test.js'],
     languageOptions: {
-      globals: {
-        ...globals.node, // добавит process, __dirname и т.п.
-      },
+      globals: { ...globals.node },   // process и др. в тестах
     },
   },
-];
+  // браузерные глобалы для клиентского кода:
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      globals: { ...globals.browser },
+    },
+  },
+]
