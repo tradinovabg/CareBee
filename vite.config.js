85320ac41642a0ad34bb3b31ae49fee1af48a706
tradinovabg <tradinovabg@gmail.com>
2025-08-24 18:39:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CareBee/', // важно: для GitHub Pages под-путь = имя репо
+  base: '/CareBee/',           // имя репозитория на GitHub Pages
 })
