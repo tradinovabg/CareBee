23bba9d0a6a7a55f6b553f468c456920f9b81b69
tradinovabg <tradinovabg@gmail.com>
2025-08-24 23:06:30 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,21 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+
+  env: { browser: true, es2021: true },
+
+  extends: ['eslint:recommended'],
+
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+
+  overrides: [
+    {
+      files: ['test/**', '**/*.test.js'],
+      env: { node: true },                 // включает Node-глобалы в тестах
+      globals: { process: 'readonly' },    // на всякий случай явно разрешаем process
+      // при желании можно полностью вырубить правило:
+      // rules: { 'no-undef': 'off' },
+    },
+  ],
+};
 
