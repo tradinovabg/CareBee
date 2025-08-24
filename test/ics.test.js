22e19aa4f8ae7d0cada0081e2bb0daa7a509f78d
tradinovabg <tradinovabg@gmail.com>
2025-08-24 10:31:25 +0200

    Update ics.test.js

--- test/ics.test.js
+++ test/ics.test.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 import { test } from 'node:test'
 import assert from 'node:assert/strict'
 import { icsEscape, buildICSEvent, toICSDateTimeUTC, toUTCStamp } from '../src/lib/ics.js'
