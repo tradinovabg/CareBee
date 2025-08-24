e3deb7211155bb28aeb25104153fd362bad863e6
tradinovabg <tradinovabg@gmail.com>
2025-08-25 00:08:40 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
-import './i18n.js'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
