8098ad3f7438c9b4e080b4cfe6b16dd98dae6884
tradinovabg <tradinovabg@gmail.com>
2025-08-25 18:04:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // ОБЯЗАТЕЛЬНО для GitHub Pages на /CareBee/
   base: '/CareBee/',
   plugins: [react()],
 })
