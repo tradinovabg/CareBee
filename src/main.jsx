5b31354fb70734e2770aa9c9e15a2ea1a1d6d265
tradinovabg <tradinovabg@gmail.com>
2025-08-25 10:50:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./i18n.js"; // см. шаг 4
+import "./i18n.js";   // 👈 просто подключаем i18n, без лишнего кода
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
