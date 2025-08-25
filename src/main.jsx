726985d4410df659d312abede01968cf196552c0
tradinovabg <tradinovabg@gmail.com>
2025-08-25 18:05:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./i18n.js"; // инициализация i18n ДО использования useTranslation
+import "./i18n.js";
 
-// для Vite/Pages: будет "/CareBee/"
 const basename = import.meta.env.BASE_URL || "/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
