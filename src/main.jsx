b5fba8aa07654f79c2284f2d9c6ccdfc3abae8d1
tradinovabg <tradinovabg@gmail.com>
2025-08-26 22:28:15 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
-import "./index.css";
-
+// pages/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./i18n.js";
-
-const basename = import.meta.env.BASE_URL || "/";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
