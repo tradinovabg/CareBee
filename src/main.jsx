63f4c26567c1ea6127599f4b9d42122510f02e8a
tradinovabg <tradinovabg@gmail.com>
2025-08-27 21:31:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./AppMini"; // ВАЖНО: используем AppMini вместо старого App
+import AppMini from "./AppMini"; // ← ПОДКЛЮЧАЕМ MINI
+
+console.log("[BOOT] main.jsx • BASE_URL =", import.meta.env.BASE_URL);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppMini />
   </React.StrictMode>
 );
