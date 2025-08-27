4c8595254ae42a361ca520b7b6702c6022248581
tradinovabg <tradinovabg@gmail.com>
2025-08-27 19:38:02 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-
-console.log("[CareBee] boot main.jsx");
+import App from "./AppMini"; // ВАЖНО: используем AppMini вместо старого App
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
