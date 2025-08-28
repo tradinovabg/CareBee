654de5e110b19b66340250fa3a0b7f21aa507e30
tradinovabg <tradinovabg@gmail.com>
2025-08-28 17:36:13 +0200

    feat: style language switcher

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,22 +1,24 @@
 import { useTranslation } from 'react-i18next'
+
 export default function LanguageSwitcher() {
   const { i18n } = useTranslation()
-  const current = (i18n.resolvedLanguage || i18n.language || 'en').slice(0,2)
+  const current = (i18n.resolvedLanguage || i18n.language || 'en').slice(0, 2)
+
+  const changeLanguage = (e) => {
+    const lang = e.target.value
+    i18n.changeLanguage(lang)
+    localStorage.setItem('lng', lang)
+  }
+
   return (
-    <label>
-      Language
-      <select
-        value={current}
-        onChange={(e) => {
-          const newLang = e.target.value
-          i18n.changeLanguage(newLang)
-          localStorage.setItem("lng", newLang)
-        }}
-      >
-        <option value="en">EN</option>
-        <option value="ru">RU</option>
-        <option value="fr">FR</option>
-      </select>
-    </label>
+    <select
+      className="select select-bordered"
+      value={current}
+      onChange={changeLanguage}
+    >
+      <option value="en">EN</option>
+      <option value="ru">RU</option>
+      <option value="fr">FR</option>
+    </select>
   )
 }
