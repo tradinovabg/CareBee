aea6908af3b90f72ae8d1b6e504ef0a7de0800dd
tradinovabg <tradinovabg@gmail.com>
2025-08-29 17:44:13 +0000

    Revert "Update vite.config.js"
    This reverts commit 25b38a76643db8ca2c8e7d74d12a8249bc85a1f5.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // ВАЖНО: имя репозитория со слешами
-  base: '/CareBee/',
+  base: '/CareBee/', // <— ВАЖНО: имя репозитория, со слешами
   plugins: [react()],
-});
+})
