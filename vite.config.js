6a8ebf30cc9a01e0c1e893d6cc65d17b8d01e834
tradinovabg <tradinovabg@gmail.com>
2025-08-30 20:04:32 +0000

    fix(pages): base=/CareBee and use HashRouter for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CareBee/', // имя репозитория со слешами
+  // сайт публикуется под /CareBee/
+  base: '/CareBee/',
   plugins: [react()],
-});
+})
